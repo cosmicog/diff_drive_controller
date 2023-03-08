2891599d54ff396c4aa4a8ae746e2735ae8b06a7
berkeunlu <berkeunlu@windowslive.com>
2023-03-08 11:16:57 +0300

    Add emergency brake to dynamic params info

--- include/diff_drive_controller/diff_drive_controller.h
+++ include/diff_drive_controller/diff_drive_controller.h
@@ -231,7 +231,11 @@ namespace diff_drive_controller{
            << "\tPublication parameters:\n"
            << "\t\tPublish executed velocity command: " << (params.publish_cmd?"enabled":"disabled") << "\n"
            << "\t\tPublication rate: " << params.publish_rate                 << "\n"
-           << "\t\tPublish frame odom on tf: " << (params.enable_odom_tf?"enabled":"disabled");
+           << "\t\tPublish frame odom on tf: " << (params.enable_odom_tf?"enabled":"disabled")
+           //
+           << "\tVelocity parameters:\n"
+           << "\t\tEmergency brake: " << (params.emergency_brake?"enabled":"disabled");
+
 
         return os;
       }
